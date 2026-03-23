f3121c079c4d3a97567ab6809b373181e1df7244
Grizzly <gabilomtatidze@gmail.com>
2026-03-23 12:57:22 +0400

    add error page

--- gegmio/app/leyout/errorPage.tsx
+++ gegmio/app/leyout/errorPage.tsx
@@ -1,21 +1,30 @@
-
-
 export default function ErrorPage() {
-
     return (
-        <>
-            <div className="w-full flex justify-center sticky top-0 z-30 ">
-                <div className="text-white flex justify-between items-center max-w-7xl w-full px-4 py-5 md:px-[100px]">
-                    <a href="/">
-                        <div className="flex items-center gap-3 cursor-pointer">
-                            <div className="w-[42px] h-[42px] border border-[#2b2b2b] rounded-full flex justify-center items-center">
-                                <img src="/images/arrow_left.svg" alt="back" />
-                            </div>
-                            <h3 className="text-[#a7a7a7]">უკან დაბრუნება</h3>
-                        </div>
-                    </a>
-                </div>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-[#0F0F0F] text-white px-4">
+
+            <div className="absolute top-5 left-0 w-full max-w-7xl px-4 md:px-[100px]">
+                <a href="/" className="flex items-center gap-3 w-fit">
+                    <div className="w-[42px] h-[42px] border border-[#2b2b2b] rounded-full flex justify-center items-center">
+                        <img src="/images/arrow_left.svg" alt="back" />
+                    </div>
+                    <h3 className="text-[#a7a7a7]">უკან დაბრუნება</h3>
+                </a>
             </div>
-        </>
-    )
+
+            <div className="flex flex-col items-center text-center gap-6">
+
+                <img src="/images/error_image.svg" alt="404 error" className="max-w-[320px] md:max-w-[400px]" />
+
+                <p className="text-[#a7a7a7] text-sm md:text-base max-w-md">
+                    ბოდიშს გიხდით შეფერხებისთვის გვერდი ვერ მოიძებნა
+                </p>
+
+                <a href="/">
+                    <button className="bg-[#F94B00] hover:bg-orange-600 transition px-[16px] py-[12px] rounded-xl font-medium">
+                        მთავარი გვერდზე დაბრუნება
+                    </button>
+                </a>
+            </div>
+        </div>
+    );
 }
