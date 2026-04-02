34efbe9e309dd7e7f6e60375662a5b901e35cb03
Grizzly <gabilomtatidze@gmail.com>
2026-04-03 03:16:54 +0400

    write accsess for user

--- gegmio/proxy.ts
+++ gegmio/proxy.ts
@@ -1,8 +1,27 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import createMiddleware from 'next-intl/middleware'
+import { routing } from './i18n/routing'
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing)
+
+export function proxy(req: NextRequest) {
+  const token = req.cookies.get("accessToken")?.value
+  const url = req.nextUrl.clone()
+
+  if (token && ["/auth/login", "/auth/registration", "/auth/otp"].includes(url.pathname)) {
+    url.pathname = "/"
+    return NextResponse.redirect(url)
+  }
+
+  if (!token && ["/page/profile", "/page/dashboard"].includes(url.pathname)) {
+    url.pathname = "/"
+    return NextResponse.redirect(url)
+  }
+
+  return intlMiddleware(req)
+}
 
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)']
-};
+  matcher: ["/((?!api|_next|.*\\..*).*)"]
+}
